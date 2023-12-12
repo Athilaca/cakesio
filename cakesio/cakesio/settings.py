e6373a9169584457b73d1afaf2bd63443aa4ab55
Athila <athilaca26@gmail.com>
2023-12-12 14:56:42 +0530

    files added

--- cakesio/cakesio/settings.py
+++ cakesio/cakesio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
-
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-almrk^vz$e3xd!girm==bmtohblb^=f-!bqhgm+ozny6%4k9a8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'cakesio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cakesioes',
-        'USER': 'postgres',
-        'PASSWORD': 'Athila@26',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432',
+        
     }
 }
 
@@ -145,7 +145,7 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static')
 
 ]
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 MESSAGE_TAGS={
 
@@ -157,10 +157,10 @@ MEDIA_ROOT=BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587  # Use the correct port for your SMTP server
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'athilaca26@gmail.com'
-EMAIL_HOST_PASSWORD = 'aokl pjrr lwqc snju'
+EMAIL_BACKEND =config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
