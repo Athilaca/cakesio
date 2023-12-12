e0a6cb2dafb34ce9cea652e6375e9bc557dd7391
Athila <athilaca26@gmail.com>
2023-12-12 17:40:46 +0530

    assert change to static

--- cakesio/cakesio/settings.py
+++ cakesio/cakesio/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -145,7 +145,7 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'cakesio/static')
 
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets/')
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 
 MESSAGE_TAGS={
 
